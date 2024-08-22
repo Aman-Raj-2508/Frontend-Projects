6b6a4feedeab5d5a9e0080e0253f7c2c08f02f94
Aman Singh <amansinghbhumihar2510@gmail.com>
2024-08-22 10:54:04 +0530

    Added js

---
+++ ToDo App/index.js
@@ -0,0 +1,6 @@
+console.log("WElcome to my todo app");
+
+let getTodosButton = document.getElementById('get-todos');//stores object
+getTodosButton.addEventListener("click", () => {
+    console.log("clicked");
+})
